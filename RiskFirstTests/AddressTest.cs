2660756e20946438529b06c81129bfd65d9e65d2
vancore295 <silverfire295@gmail.com>
2019-07-11 20:40:09 -0400

    added in test project

---
+++ RiskFirstTests/AddressTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RiskFirstTests
+{
+    public class AddressTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ RiskFirstTests/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using RiskFirstTest;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RiskFirstTests
+{
+    class TestClientProvider
+    {
+        public HttpClient Client { get; private set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
